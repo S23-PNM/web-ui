7d63d38d71869450b9c78fab6d991e9ad9665ebf
Yeewilliam2004 <130953844+Yeewilliam2004@users.noreply.github.com>
2023-04-23 01:41:33 -0700

    New Buttons

--- src/components/time-tab.js
+++ src/components/time-tab.js
@@ -1,4 +1,4 @@
-function LocationTab(props) {
+function TimeTab(props) {
     return (
         <div className="inline-block text-sm font-bold mb-4 rounded-full border border-black text-black py-2 px-4 hover:bg-black hover:text-white transition-colors duration-200" style={{padding: '10px'}}>
             {props.title}
@@ -7,4 +7,4 @@ function LocationTab(props) {
     )
 }
 
-export default LocationTab;
+export default TimeTab;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,28 @@
 import LocationTab from "../components/location-tab";
 import StatusTab from "../components/status-tab";
+import OccupancyTab from "../components/occupancy-tab";
+import TimeTab from "../components/time-tab";
 
 export default function Home() {
 
   return (
     <main>
       <h1 className="text-6xl leading-tight font-bold">Dashboard</h1>
-      <button className="text-sm font-bold mb-4 rounded border border-black text-black py-2 px-4 hover:bg-black hover:text-white transition-colors duration-200">Spartan Food Pantry</button>
-      <button className="text-sm font-bold mb-4 rounded border border-black text-black py-2 px-4 rounded-none hover:bg-black hover:text-white transition-colors duration-200">The Commons</button>
+      <LocationTab title="Spartan Food Pantry" />
+      <LocationTab title="The Commons" />
       <div className="mt-4">
-        <StatusTab title="testing" />
+        <StatusTab title="testing" /> 
+      </div>
+      <div className="mt-4">
+      <OccupancyTab title="Recent Occupancy" /> 
+
+      </div>
+      <div className="mt-4">
+      <TimeTab title="1 Month" /> 
+      <TimeTab title="1 Week" /> 
+      <TimeTab title="1 Day" /> 
+      <TimeTab title="12 Hours" /> 
+      <TimeTab title="1 Hour" /> 
       </div>
     </main>
   )
