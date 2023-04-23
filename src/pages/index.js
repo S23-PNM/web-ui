ae3ecd561fff83d143750c96b4fbcecc161f498a
Yeewilliam2004 <130953844+Yeewilliam2004@users.noreply.github.com>
2023-04-22 23:49:41 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,15 @@ import LocationTab from "../components/location-tab";
 import StatusTab from "../components/status-tab";
 
 export default function Home() {
-  
+
   return (
     <main>
       <h1 className="text-6xl leading-tight font-bold">Dashboard</h1>
-      <LocationTab title="Spartan Food Pantry" />
-      <LocationTab title="The Commons" />
+      <button className="text-3xl font-bold mb-4 rounded-full bg-blue-500 text-white py-3 px-6">Spartan Food Pantry</button>
+      <button className="text-3xl font-bold mb-4 rounded-full bg-blue-500 text-white py-3 px-6">The Commons</button>
       <div className="mt-4">
         <StatusTab title="testing" />
       </div>
-      
     </main>
   )
 }
