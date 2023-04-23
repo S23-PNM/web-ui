b551254ab4d6f57c61f54c041031a1e6084fd32e
Yeewilliam2004 <130953844+Yeewilliam2004@users.noreply.github.com>
2023-04-23 00:13:25 -0700

    Hover color

--- src/components/location-tab.js
+++ src/components/location-tab.js
@@ -1,6 +1,6 @@
 function LocationTab(props) {
     return (
-        <div className="inline-block bg-blue-600 rounded-full text-white font-bold" style={{padding: '10px'}}>
+        <div className="inline-block shadow-xl bg-blue-600 rounded-full text-white font-bold" style={{padding: '10px'}}>
             {props.title}
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-6xl leading-tight font-bold">Dashboard</h1>
-      <button className="text-sm font-bold underline mb-4 rounded bg-blue-500 text-white py-2 px-4">Spartan Food Pantry</button>
-      <button className="text-sm font-bold underline mb-4 rounded bg-blue-500 text-white py-2 px-4 rounded-none">The Commons</button>
+      <button className="text-sm font-bold mb-4 rounded border border-black text-black py-2 px-4 hover:bg-black hover:text-white transition-colors duration-200">Spartan Food Pantry</button>
+      <button className="text-sm font-bold mb-4 rounded border border-black text-black py-2 px-4 rounded-none hover:bg-black hover:text-white transition-colors duration-200">The Commons</button>
       <div className="mt-4">
         <StatusTab title="testing" />
       </div>
