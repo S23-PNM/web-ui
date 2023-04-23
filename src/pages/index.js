0f0fd54e7330603e44474fbb693ab393cf6deb2b
Yeewilliam2004 <130953844+Yeewilliam2004@users.noreply.github.com>
2023-04-23 00:00:28 -0700

    Nnew

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-6xl leading-tight font-bold">Dashboard</h1>
-      <button className="text-3xl font-bold mb-4 rounded-full bg-blue-500 text-white py-3 px-6">Spartan Food Pantry</button>
-      <button className="text-3xl font-bold mb-4 rounded-full bg-blue-500 text-white py-3 px-6">The Commons</button>
+      <button className="text-sm font-bold underline mb-4 rounded bg-blue-500 text-white py-2 px-4">Spartan Food Pantry</button>
+      <button className="text-sm font-bold underline mb-4 rounded bg-blue-500 text-white py-2 px-4 rounded-none">The Commons</button>
       <div className="mt-4">
         <StatusTab title="testing" />
       </div>
