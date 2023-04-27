1e5700ab22579bbdd0adf59bfb346541e91a7883
Siddharth <hi@sid.fyi>
2023-04-26 22:27:50 -0700

    solve graph rendering bug

--- src/components/graph.jsx
+++ src/components/graph.jsx
@@ -7,11 +7,11 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-function GraphTab({ data }) {
+function GraphTab({ data, type }) {
   return (
     <ResponsiveContainer height={240}>
       <LineChart data={data} fontSize={14} fontWeight={500}>
-        <Line strokeWidth={4} stroke="#3F3F46" dataKey="count" />
+        <Line strokeWidth={4} stroke="#3F3F46" dataKey={type} />
         <Tooltip />
         <XAxis
           height={48}
@@ -25,12 +25,12 @@ function GraphTab({ data }) {
         <YAxis
           width={48}
           label={{
-            value: "# of people",
+            value: `# of people ${type}ing`,
             angle: -90,
             position: "insideLeft",
             textAnchor: "middle",
           }}
-          dataKey="count"
+          dataKey={type}
         />
       </LineChart>
     </ResponsiveContainer>
--- src/components/occupancy-tab.jsx
+++ src/components/occupancy-tab.jsx
@@ -3,6 +3,7 @@ import TimeTab from "./time-tab";
 import GraphTab from "./graph";
 
 function OccupancyTab({ title, location }) {
+  const [type, setType] = useState("enter");
   const [data, setData] = useState();
 
   useEffect(() => {
@@ -16,42 +17,32 @@ function OccupancyTab({ title, location }) {
     const locationData = data.find((d) => d.location === location);
     return locationData.times
       .sort((a, b) => a.time - b.time)
-      .map((d) => ({
+      .map(({ time, ...d }) => ({
         time: `${new Date(
-          locationData.now + d.time * 60 * 60 * 1000
+          locationData.now + time * 60 * 60 * 1000
         ).getHours()}:00`,
-        count: d.count,
+        ...d,
       }));
   }, [data, location]);
 
   return (
     <div className="w-full max-w-3xl p-4 inline-block bg-white-500 bg-opacity-75 text-gray-700 font-bold border-gray-200 border rounded-lg bg-gray-50">
       <p>{title}</p>
-      {/* <div className="my-4 space-x-2"> */}
-      {/*   <TimeTab */}
-      {/*     active={period === "1m"} */}
-      {/*     onClick={() => setPeriod("1m")} */}
-      {/*     title="1 Month" */}
-      {/*   /> */}
-      {/*   <TimeTab */}
-      {/*     active={period === "1w"} */}
-      {/*     onClick={() => setPeriod("1w")} */}
-      {/*     title="1 Week" */}
-      {/*   /> */}
-      {/*   <TimeTab */}
-      {/*     active={period === "1d"} */}
-      {/*     onClick={() => setPeriod("1d")} */}
-      {/*     title="1 Day" */}
-      {/*   /> */}
-      {/*   <TimeTab */}
-      {/*     active={period === "1h"} */}
-      {/*     onClick={() => setPeriod("1h")} */}
-      {/*     title="1 Hour" */}
-      {/*   /> */}
-      {/* </div> */}
+      <div className="my-4 space-x-2">
+        <TimeTab
+          active={type === "enter"}
+          onClick={() => setType("enter")}
+          title="Enter"
+        />
+        <TimeTab
+          active={type === "exit"}
+          onClick={() => setType("exit")}
+          title="Exit"
+        />
+      </div>
       <div className="h-60">
         {data ? (
-          <GraphTab data={dataToRender} />
+          <GraphTab data={dataToRender} type={type} />
         ) : (
           <p className="text-4xl">Loading...</p>
         )}
